ae4b6836e7f8d5a73c16f549830ddd9fe5e66b7e
Deaung <a76650051@gmail.com>
2025-04-21 00:39:20 +0900

    feat:20250421 사진자료 다찾았고 조정후 삽입하면 끝?

---
+++ src/detail/central/Central.jsx
@@ -0,0 +1,80 @@
+import Footer from "../../footer/Footer.jsx";
+import Header from "../../header/Header.jsx";
+// import SwiperImage from "../../component/ImgMove.tsx";
+import "./Central.css";
+// import SlideImg from '../../component/Slideimg.jsx'
+
+import centralCafeImg1 from "../../img/centralCafe1.jpg";
+import centralCafeImg2 from "../../img/centralCafe2.jpg";
+import centralCafeImg3 from "../../img/centralCafe3.jpg";
+import centralCafeImg4 from "../../img/centralCafe4.jpg";
+// import { images_1, images_2, images_3, images_4 } from "../../component/centerimgs.jsx";
+
+
+export default function Central() {
+    return (
+        <div className="central-cafe">
+            <Header />
+            {/* <img src="src\img\중부.png" alt="이미지" className="background"/> */}
+            
+                
+                <div className="central-part1">
+                    <a href="https://naver.me/5Q3mYIGJ" target="_blank" rel="noopener noreferrer">
+                        <img src={centralCafeImg1} alt="이내 무목 이미지"/>
+                    </a>
+                    <div className="part-1-detail">
+                    <p>이내 무목</p>
+                    <p>인공폭포가 이쁘고, 인테리어가 독특한 곳</p>
+                    <p>#폭포</p>
+                    </div>
+                </div>
+
+                <div className="central-part2">
+                    <a href="https://naver.me/xJiRmdcK" target="_blank" rel="noopener noreferrer">
+                        <img src={centralCafeImg2} alt="카페 뱅커 이미지" />
+                        
+                    </a>
+                    <div className="part-2-detail">
+                    <p>뱅커</p>
+                        <p>쿠키와 빵 종류가 다양하고, 넓음</p>
+                        <p>#크로플</p>
+                    </div>
+                    
+                </div>
+
+            
+            
+            <div className="central-part3">
+                
+                
+                <a href="https://naver.me/xeAOhY3e" target="_blank" rel="noopener noreferrer">
+                    <img src={centralCafeImg3} alt="카페 이디야커피 이미지" />
+                    
+                </a>
+                <div className="part-3-detail">
+                <p>이디야커피 부산지오플레이스점</p>
+                <p>사장님이 친절하고, 분위기가 조용함</p>
+                <p>#공부하기 좋은 카페</p>
+                </div>
+            </div>
+
+            <div className="central-part4">
+                
+                <a href="https://naver.me/Gcts7ekh" target="_blank" rel="noopener noreferrer">
+                    <img src={centralCafeImg4} alt="카페 오프커스 이미지" />
+                    
+                </a>
+                <div className="part-4-detail">
+                <p>오프커스</p>
+                <p>서면에서 보기 드문 대형 카페</p>
+                <p>#커피가 맛있음 #서면 대형 카페</p>
+                </div>
+                
+            </div>
+        
+            
+
+            <Footer />
+        </div>
+    );
+}
---
+++ src/home/Home.jsx
@@ -0,0 +1,79 @@
+// 페이지 이동
+import { useNavigate } from "react-router-dom";
+
+import PageMove from "../component/PageMovie.jsx";
+import Footer from "../footer/Footer.jsx";
+import Header from "../header/Header.jsx";
+
+import "./Home.css";
+
+export default function Home() {
+    const navigate = useNavigate();
+        
+    return(
+        // 페이지 전체
+        <div className="home-container">
+            {/* header */}
+            <Header />
+
+            {/* body */}
+            <div className="home-body">
+                <div className="buttons">
+                <button className="central-btn" onClick={() => PageMove(navigate, "central")}>중부권</button>
+                <button className="eastern-btn" onClick={() => PageMove(navigate, "eastern")}>동부권</button>
+                <button className="southern-btn" onClick={() => PageMove(navigate, "southern")}>남부권</button>
+                <button className="western-btn" onClick={() => PageMove(navigate, "western")}>서부권</button>
+                </div>
+                {/* 지도 이미지 */}
+                        <svg viewBox="0 0 953 700" className="busan-svg-map" xmlns="http://www.w3.org/2000/svg">
+                    {/* 부산 지도 이미지 */}
+                    <image href="src\img\busan-1.png" x="0" y="0" width="953" height="700" />
+
+                    {/* 중부권 */}
+                    <polygon 
+                        points="451,229 481,201 566,174 593,195 576,246 585,261 603,265 618,285 592,321 578,323 577,340 564,360 569,414 561,418 541,422 536,441 535,452 516,452 503,469 480,476 459,482 443,479 418,504 401,484 412,460 400,428 411,398 450,380 475,334 456,322 459,303 438,263"
+                        fill="transparent"
+                        stroke="#f00"
+                        strokeWidth="2"
+                        className="region central"
+                        onClick={() => PageMove(navigate, "central")}
+                    />
+
+                    {/* 동부권 */}
+                    <polygon 
+                        points="600,460 593,434 572,412 565,359 579,347 581,324 598,321 612,301 619,285 603,264 583,255 588,226 593,198 562,170 585,130 602,91 593,72 632,68 676,89 706,74 713,48 701,34 705,27 725,28 737,42 826,28 850,60 858,71 860,92 858,114 890,134 893,155 855,150 828,157 825,206 821,248 793,347 773,382 742,413 707,449 674,468 631,471"
+                        fill="transparent"
+                        stroke="#00f"
+                        strokeWidth="2"
+                        className="region eastern"
+                        onClick={() => PageMove(navigate, "eastern")}
+                    />
+
+                    {/* 서부권 */}
+                    <polygon 
+                        points="446,229 400,236 400,258 380,300 300,333 276,329 244,339 238,330 219,326 205,345 186,349 161,371 171,389 174,419 185,441 127,431 80,450 33,446 33,468 45,490 80,508 99,541 56,559 86,585 154,604 182,598 233,586 270,596 318,674 355,671 382,668 385,611 402,597 366,533 383,490 399,486 401,453 393,429 406,395 446,376 472,341 449,325 455,303 432,264 448,240"
+                        fill="transparent"
+                        stroke="#0a0"
+                        strokeWidth="2"
+                        className="region western"
+                        onClick={() => PageMove(navigate, "western")}
+                    />
+
+                    {/* 남부권 */}
+                    <polygon 
+                        points="567,411 592,434 600,459 569,491 587,512 599,539 581,568 543,568 501,546 476,533 476,564 504,580 526,620 543,644 536,666 501,646 481,639 460,616 427,594 415,635 397,645 387,613 402,602 399,573 386,543 369,532 376,519 385,491 401,487 416,510 436,490 454,481 477,482 505,470 519,455 539,456 543,431"
+                        fill="transparent"
+                        stroke="#fa0"
+                        strokeWidth="2"
+                        className="region southern"
+                        onClick={() => PageMove(navigate, "southern")}
+                    />
+                    </svg>
+                        <img src="src\img\center-point.png" alt="중앙포인트" className="hover-image" />
+
+                    </div>
+            {/* footer */}
+            <Footer />
+        </div>
+    );
+}
